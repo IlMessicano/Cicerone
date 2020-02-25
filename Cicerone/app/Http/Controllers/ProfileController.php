027382e78189267128f601efc80334ca9d1ec44f
dtErik <61144037+dtErik@users.noreply.github.com>
2020-02-25 11:49:21 +0100

    Gestione profilo, con upload immagine

--- Cicerone/app/Http/Controllers/ProfileController.php
+++ Cicerone/app/Http/Controllers/ProfileController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class ProfileController extends Controller
 {
@@ -36,7 +38,35 @@ class ProfileController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'img' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:6000',
+        ]);
+
+
+        //Upload del file
+        if ($request->hasFile('img')) {
+            //Acquisisco il nome del file
+            $filenameWithExt = $request->file('img')->getClientOriginalName();
+            //Prendo il nome del file senza estensione
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+
+            //Prendo solo l'estensione
+            $extension = $request->file('img')->getClientOriginalExtension();
+
+            //Creo il nome del file con il timestamp
+            $fileNameToStore = $filename . '_' . time() . '.' . $extension;
+            //Upload
+            $path = $request->file('img')->storeAs('public/profileImg', $fileNameToStore);
+        }
+
+        $id = auth()->user()->id;
+        $user = User::find($id);
+        //Se avevo caricato un immagine, la elimino per caricare la nuova
+        if($user->imgProfilo != NULL)
+            Storage::delete('public/profileImg/' . $user->imgProfilo);
+        $user->imgProfilo = $fileNameToStore;
+        $user->save();
+        return view('profile.index')->with('success', 'Immagine caricata')->with('user',$user);
     }
 
     /**
--- Cicerone/resources/views/profile/index.blade.php
+++ Cicerone/resources/views/profile/index.blade.php
@@ -1,5 +1,124 @@
 @extends('layouts.app')
 
 @section('content')
-    
+<div class="container">
+    <div class="shadow p-4 m-4 bg-white">
+        <br>
+
+
+        <div class="row">
+
+            <div class="col-md-5 order-md-1 mb-4">
+                <h4 class="d-flex justify-content-between align-items-center mb-3">
+                    <span class="text-muted">Immagine del profilo</span>
+
+                </h4>
+                <div class="card p-2">
+                    <img src="/storage/profileImg/{{$user->imgProfilo}}" class="img-responsive m-3" width="350"
+                        height="300">
+                    <div class="input-group">
+                        <div class="input-group-append">
+                            {!! Form::open(['action'=>'ProfileController@store','method' => 'POST','enctype' =>
+                            'multipart/form-data'])!!}
+
+                            <div class="custom-file">
+                                {{ Form::file('img',['class' => 'input-group-text']) }}
+                            </div>
+                            <div class="input-group-append">
+                                {{Form::submit('Upload',['class' => 'btn btn-primary'])}}
+
+                            </div>
+
+                            {!! Form::close()!!}
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="card p-2 my-2 text-center">
+                <div class="row ">
+                    <div class="col-md-6 mb-3">
+                        <img src="/img/thumbsUp.png" width="30" height="30">
+                        Voti positivi: {{$user->votiPos}}
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <img src="/img/thumbsDown.png" width="30" height="30">
+                        Voti negativi: {{$user->VotiNeg}}
+                    </div>
+                </div>
+                </div>
+            </div>
+
+
+
+            <div class="col-md-7 order-md-2">
+                <h4 class="mb-3">Modifica profilo</h4>
+                <form class="needs-validation" novalidate="">
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="name">Nome</label>
+                            <input type="text" class="form-control" id="name" placeholder="{{$user->name}}"
+                                value="{{$user->name}}" required="">
+
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="cognome">Cognome</label>
+                            <input type="text" class="form-control" id="cognome" placeholder="{{$user->cognome}}"
+                                value="{{$user->cognome}}" required="">
+
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="email">Email </label>
+                        <input type="email" class="form-control" id="email" placeholder="{{$user->email}}"
+                            value="{{$user->email}}">
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="sesso">Sesso</label>
+                            <input type="text" class="form-control" id="sesso" placeholder="{{$user->sesso}}">
+                        </div>
+
+                        <div class="col-md-6 mb-3">
+                            <label for="dataNascita">Data di nascita</label>
+                            <input type="date" class="form-control" id="dataNascita" value="{{$user->dataNascita}}">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 mb-3">
+                            <label for="nazionalita">Nazionalità</label>
+                            <input type="text" class="form-control" id="nazionalita" placeholder="{{$user->nazionalita}}"
+                                required="">
+
+                        </div>
+
+                        <div class="col-md-4 mb-3">
+                            <label for="cittaResidenza">Città di residenza</label>
+                            <input type="text" class="form-control" id="cittaResidenza"
+                                placeholder="{{$user->cittaResidenza}}">
+                        </div>
+
+                        <div class="col-md-4 mb-3">
+                            <label for="nazioneResidenza">Nazione di residenza</label>
+                            <input type="text" class="form-control" id="nazioneResidenza"
+                                placeholder="{{$user->nazioneResidenza}}">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="biografia">Biografia</label>
+                        <input type="textarea" class="form-control" id="biografia" placeholder="{{$user->biografia}}">
+                    </div>
+
+
+                    <hr class="mb-4">
+                    <button class="btn btn-primary btn-lg btn-block" type="submit">Aggiorna profilo</button>
+                </form>
+            </div>
+        </div>
+
+
+    </div>
+</div>
 @endsection
