223c594065ccb14a774e2667c439281b0abebc41
thavide96 <61144081+thavide96@users.noreply.github.com>
2020-02-29 11:50:56 +0100

    Immagine background

--- Cicerone/resources/views/welcome.blade.php
+++ Cicerone/resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div
-    style="background-image:url('https://besthqwallpapers.com/Uploads/16-4-2018/48522/thumb2-travel-concepts-wooden-plane-seashells-summer-travel-blue-boards.jpg'); background-repeat: no-repeat; background-size: cover;"
+    style="background-image:url('/img/...'); background-repeat: no-repeat; background-size: cover;"
     class="jumbotron w-100 bg-black">
     <div class="container">
         <section class="search-banner text-white py-1" id="search-banner">
