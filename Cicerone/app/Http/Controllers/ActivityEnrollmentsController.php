338fc7724eb78e8807553a70728c087688f29dcb
dtErik <61144037+dtErik@users.noreply.github.com>
2020-04-07 17:25:08 +0200

    Update ActivityEnrollmentsController.php

--- Cicerone/app/Http/Controllers/ActivityEnrollmentsController.php
+++ Cicerone/app/Http/Controllers/ActivityEnrollmentsController.php
@@ -76,20 +76,26 @@ class ActivityEnrollmentsController extends Controller
     {
         $plan = activity_plannings::find($planId);
         $user = User::find(Auth::user()->id);
-        if ($user->balance >= $plan->cost) {
-            $user->balance =   $user->balance - $plan->cost;
-            Activity_Enrollments::create([
-                'PlanningId' => $plan->planningId,
-                'User' => Auth::user()->id,
-                'enrollmentDate' => Carbon\Carbon::now(),
-            ]);
-            $user->save();
+        if($plan->numPartecipants < $plan->maxPartecipants) {
+            if ($user->balance >= $plan->cost) {
+                $user->balance = $user->balance - $plan->cost;
+                Activity_Enrollments::create([
+                    'PlanningId' => $plan->planningId,
+                    'User' => Auth::user()->id,
+                    'enrollmentDate' => Carbon\Carbon::now(),
+                ]);
+                $plan->numPartecipants = $plan->numPartecipants + 1;
+                $plan->save();
+                $user->save();
 
-            return redirect('/attivita/' . $plan->activity_id)->with('success', 'Attività prenotata');
+                return redirect('/attivita/' . $plan->activity_id)->with('success', 'Attività prenotata');
 
 
-        } else {
-            return redirect('/attivita/' . $plan->activity_id)->with('error', 'Saldo insufficiente per prenotare questa attività');
+            } else {
+                return redirect('/attivita/' . $plan->activity_id)->with('error', 'Saldo insufficiente per prenotare questa attività');
+            }
+        }else{
+            return redirect('/attivita/' . $plan->activity_id)->with('error', 'Numero massimo di partecipanti raggiunto per questa attività');
         }
 
 
@@ -108,6 +114,9 @@ class ActivityEnrollmentsController extends Controller
 
         $activity_id = $enroll->plan->activity_id;
         $costo = $enroll->plan->cost;
+     
+        $enroll->plan->numPartecipants = $enroll->plan->numPartecipants - 1;
+        $enroll->plan->save();
         $user = User::find($enroll->User);
         $user->balance = $user->balance + $costo;
         $user->save();
